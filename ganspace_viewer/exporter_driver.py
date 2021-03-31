1abbcf9bfb10b51592f584f4012d99e1538e7e91
Phat Duong <duongp@wit.edu>
2021-03-31 18:24:03 -0400

    deleted back end stuff and moved everything outside

---
+++ ganspace_viewer/exporter_driver.py
@@ -0,0 +1,93 @@
+# import sys
+# import zmq 
+# import json
+
+# #TODO change this so that it works anywhere
+# from export import GanModel
+# from django import forms
+
+# car = GanModel(model='StyleGAN2', class_name='car', layer='style', n=1_000_000, b=10_000)
+
+# class ganspace_state(forms.Form):
+#     model_name = forms.CharField(label = 'model_name', max_length=50)
+#     layer_start_slider_value = forms.IntegerField()
+#     layer_end_slider_value = forms.IntegerField()
+
+
+# def sendDataToExporter(layer_start, layer_end, component_list):
+#     print()
+#     # print('layer start', layer_start)
+#     # print('layer end', layer_end)
+#     for i in range(len(component_list)):
+#         # print('Component',i,component_list[i])
+#         component_list[i] = float(component_list[i])
+
+#     context = zmq.Context()
+
+#     socket = context.socket(zmq.REQ)
+#     socket.connect("tcp://ip-172-31-24-131.us-east-2.compute.internal:5555")
+
+#     socket
+
+#     model = 'car'
+#     seed = '0'
+
+#     js = {
+#         "model": model,
+#         "seed": int(seed),
+#         "slider_values": component_list
+#     }
+
+#     socket.send_json(json.dumps(js))
+
+#     filename = socket.recv_string()
+#     print(filename)
+
+#     return filename
+
+
+from PIL import Image
+from pathlib import Path
+from os import sys
+import os.path
+from time import sleep
+import numpy as np
+import base64
+from io import BytesIO
+sys.path.insert(1, '/Users/duongp/ganspace-online')
+    
+def sendDataToExporter(model_name, layer_start, layer_end, component_list, seed):
+    print()
+    print('model name', model_name)
+    print('layer start', layer_start)
+    print('layer end', layer_end)
+    for i in range(len(component_list)):
+        print('Component',i,component_list[i])
+    print('seed', seed)
+    
+    value = float(component_list[0])*10
+
+    path = os.path.join(os.path.dirname(os.path.dirname(__file__)),'static/media/Images','shoe_even.jpg')
+    even = Image.open(path)
+    path = os.path.join(os.path.dirname(os.path.dirname(__file__)),'static/media/Images','shoe_odd.jpg')
+    odd = Image.open(path)
+    buffered = BytesIO()
+    
+    path = os.path.join(os.path.dirname(os.path.dirname(__file__)),'static/media/Images','test.jpg')
+    if (int(value) % 2 == 0):
+        even.save(path)
+        #even.save(buffered, format="JPEG")
+    else:
+        #odd.save(buffered, format="JPEG")
+        odd.save(path)
+    #img_str = base64.b64encode(buffered.getvalue())
+    #return img_str
+    #sleep(0.1)
+    
+def save_component(layer_start, layer_end, component_number, component_name):
+    print(layer_start, layer_end, component_number, component_name)
+
+def generate_new_seed():
+    return np.random.randint(np.iinfo(np.int32).max - 1)
+
+
---
+++ ganspace_viewer/views.py
@@ -0,0 +1,122 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.http import JsonResponse
+from .models import *
+from .exporter_driver import *
+from .forms import *
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    return render(request, "home.html", {})
+from django.shortcuts import render
+
+def login_view(request):
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username =username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+
+    context = {}
+    return render(request, "login.html",context)
+
+#TODO: implement this
+def logoutUser(request):
+    return redirect('login')
+
+def register_view(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account created for ' + user)
+            
+            return redirect('login')
+
+
+    context= { 'form': form}
+    return render(request, "register.html",context)
+
+# Create your views here.
+
+#Admin Views
+def manage_users_view(request, *args, **kwargs):
+    return render(request, "admin_manage_users.html", {})
+
+def guides_view(request, *args, **kwargs):
+    return render(request, "admin_guides.html", {})
+
+def account_view(request, *args, **kwargs):
+    return render(request, "admin_account.html", {})
+
+
+def data_view(request, *args, **kwargs):
+    return render(request, "admin_data.html", {})
+
+def new_models_view(request, *args, **kwargs):
+    return render(request, "admin_new_models.html", {})
+
+def models_view(request ,*args, **kwargs):
+    #model_list = kwargs.models
+    model_list = ganspace_model.objects.values_list('className', flat = True)
+    my_context = {
+        "model_list" : model_list , #TODO: implement exporter
+    }
+    
+    return render(request, "admin_models.html", my_context)
+
+def user_gallery_view(request, *args, **kwargs):
+    return render(request, "admin_user_gallery.html", {})
+
+def ganspace_view(request, model_name):
+    model = ganspace_model.objects.get(className = model_name)
+
+    # seed = request.POST.get('seed')
+    # layer_start = request.POST.get('layer_start')
+    # print(seed, layer_start)
+
+    #print(request.POST.get('layer_start'))
+    
+    if request.method == 'POST' and request.is_ajax():
+        model_name = request.POST.get('model_name')
+        layer_start = request.POST.get('layer_start')
+        layer_end = request.POST.get('layer_end')
+        component_list = request.POST.getlist('component_sliders[]')
+        response_type = request.POST.get('type')
+        seed = request.POST.get('seed')
+
+        if (response_type == 'slider_value_update' or response_type == 'update_seed'):
+            sendDataToExporter(model_name, layer_start, layer_end, component_list, seed)
+            return JsonResponse({})
+        elif (response_type == 'resample_latent'):
+            seed = generate_new_seed()
+            sendDataToExporter(model_name, layer_start, layer_end, component_list, seed)
+            return JsonResponse({'seed': seed})
+        elif (response_type == 'save_component'):
+            component_number = request.POST.get('component_number')
+            component_name = request.POST.get('component_name')
+            save_component(layer_start, layer_end, component_number, component_name)
+
+
+    #initialize 
+    seed = generate_new_seed()
+    context = {
+        "component_list": range(10),
+        "seed": seed,
+
+    }
+    return render(request,'admin_ganspace_viewer.html', context)
